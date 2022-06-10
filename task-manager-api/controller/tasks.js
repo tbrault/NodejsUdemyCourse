5c6950c17509cc7c5b102916b6072aaf6621c9c5
tbrault <heymarin@hotmail.fr>
2022-06-10 11:59:13 +0200

    add deleteTask

--- task-manager-api/controller/tasks.js
+++ task-manager-api/controller/tasks.js
@@ -47,7 +47,18 @@ const updateTask = async (req, res) => {
   }
 };
 
-const deleteTask = async (req, res) => {};
+const deleteTask = async (req, res) => {
+  try {
+    const { id: taskId } = req.params;
+    const task = await Task.findByIdAndDelete(taskId);
+
+    if (!task) return res.status(404).json({ msg: `no id : ${taskId} in db` });
+
+    res.status(200).json({ task });
+  } catch (error) {
+    res.status(500).json({ msg: error });
+  }
+};
 
 module.exports = {
   getAllTasks,
