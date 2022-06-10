924c7f12226496ab2a665237aa3e76bf8a30d8cf
tbrault <heymarin@hotmail.fr>
2022-06-10 11:56:15 +0200

    add updateTask

--- task-manager-api/controller/tasks.js
+++ task-manager-api/controller/tasks.js
@@ -31,7 +31,21 @@ const getSingleTask = async (req, res) => {
   }
 };
 
-const updateTask = async (req, res) => {};
+const updateTask = async (req, res) => {
+  try {
+    const { id: taskId } = req.params;
+    const task = await Task.findByIdAndUpdate(taskId, req.body, {
+      new: true,
+      runValidators: true,
+    });
+
+    if (!task) return res.status(404).json({ msg: `no id : ${taskId} in db` });
+
+    res.status(200).json({ task });
+  } catch (error) {
+    res.status(500).json({ msg: error });
+  }
+};
 
 const deleteTask = async (req, res) => {};
 
