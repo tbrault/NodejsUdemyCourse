2662dd86c7498a16c29f6eb86c6cace1f9aa19be
tbrault <heymarin@hotmail.fr>
2023-03-23 17:52:10 +0100

    init app

--- store-api/app.ts
+++ store-api/app.ts
@@ -1,3 +1,24 @@
-//import modules
 import express from "express";
 const app = express();
+import connectDb from "./db/connect";
+
+import notFound from "./middleware/not-found";
+import errorHandlerMiddleware from "./middleware/error-handler";
+
+require("dotenv").config();
+
+const port = process.env.PORT || "5000";
+
+app.use(notFound);
+app.use(errorHandlerMiddleware);
+
+const start = async () => {
+  try {
+    await connectDb(process.env.MONGO_URI);
+    app.listen(port, console.log(`server is listening on port ${port}....`));
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
---
+++ store-api/db/connect.ts
@@ -0,0 +1,12 @@
+import mongoose from "mongoose";
+
+const connectDb = (url) => {
+  return mongoose.connect(url, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+  });
+};
+
+export default connectDb;
