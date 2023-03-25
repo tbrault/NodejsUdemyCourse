9aa446cd93ff90a808bd9c7bdb94d3bff714e5c6
tbrault <heymarin@hotmail.fr>
2023-03-25 19:00:44 +0100

    add skip and limit

--- store-api/controllers/products.ts
+++ store-api/controllers/products.ts
@@ -63,6 +63,12 @@ async function getAllProducts(req: Request, res: Response) {
     result = result.select(fieldsList);
   }
 
+  const limit = Number(req.query.limit) || 10;
+  const page = Number(req.query.page) || 1;
+  const skip = (page - 1) * limit;
+
+  result = result.skip(skip).limit(limit);
+
   const products = await result.exec();
   res.status(200).json({ products, total: products.length });
 }
