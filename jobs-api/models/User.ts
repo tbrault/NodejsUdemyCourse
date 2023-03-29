089f385a375a6c9277bc0d4f93e80687d5c8b4a3
tbrault <heymarin@hotmail.fr>
2023-03-29 15:56:49 +0200

    add user model

---
+++ jobs-api/models/User.ts
@@ -0,0 +1,27 @@
+import { Schema, model } from "mongoose";
+
+const User = new Schema({
+  name: {
+    type: String,
+    required: [true, "Name must be provided"],
+    maxlength: 40,
+    lowercase: true,
+    trim: true,
+  },
+  email: {
+    type: String,
+    required: [true, "Email must be provided"],
+    match: [
+      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+      "Please provide a valid email ",
+    ],
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: [true, "Password must be provided"],
+    minlength: 6,
+  },
+});
+
+export default model("User", User);
