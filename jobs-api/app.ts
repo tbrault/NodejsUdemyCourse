ac9582700c43dec7680bbc51c5bf00cc532231c1
tbrault <heymarin@hotmail.fr>
2023-04-01 16:16:53 +0200

    setup security middlewares

--- jobs-api/app.ts
+++ jobs-api/app.ts
@@ -1,6 +1,11 @@
 import express from "express";
 import * as dotenv from "dotenv";
 import "express-async-errors";
+import helmet from "helmet";
+import cors from "cors";
+import xss from "xss-clean";
+import rateLimit from "express-rate-limit";
+
 import validateAuthentication from "./middlewares/auth.js";
 import authRouter from "./routes/auth.js";
 import jobsRouter from "./routes/jobs.js";
@@ -12,6 +17,16 @@ const app = express();
 dotenv.config();
 const port = process.env.PORT || 5000;
 
+app.set("trust proxy", 1);
+app.use(
+  rateLimit({
+    windowMs: 15 * 60 * 1000,
+    max: 100,
+  })
+);
+app.use(helmet());
+app.use(cors());
+app.use(xss());
 app.use(express.json());
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/jobs", validateAuthentication, jobsRouter);
